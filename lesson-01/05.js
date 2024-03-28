72cdada579377813fbae42485dd14b7fa2eea71c
maxxizup <maksimkaxd1@gmail.com>
2024-03-28 14:33:43 +1000

    hw 04: add switch case for rule

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -13,7 +13,7 @@ switch (true) {
         console.log("У меня 100 пудов все получится");
         break;
     
-    case rule.length > 25:
+    case rule.length === 25:
         console.log("50 на 50");
         break;
 }
