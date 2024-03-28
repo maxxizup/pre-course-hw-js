1deaa51991cd553c3e8ae348e67ce02886131b62
maxxizup <maksimkaxd1@gmail.com>
2024-03-29 03:29:23 +1000

    hw 04: add switch case for rule

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -6,11 +6,11 @@ let rule = "Еще не родился тот человек, который, п
 
 switch (true) {
     case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
+        console.log("У меня 100 пудов все получится");
         break;
 
     case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
+        console.log("Все таки нет правил без исключения");
         break;
 
     default:
