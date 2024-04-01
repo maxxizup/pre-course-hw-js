42efe369e2587f9cf98be6e9c22c389ab5f2f795
maxxizup <maksimkaxd1@gmail.com>
2024-04-02 01:21:12 +1000

    hw 01: refactor doArrow() into arrow function

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,1 +1,11 @@
-// code
+// Переделай функцию на стрелочный синтаксис
+
+// function doArrow(value) {
+// 	console.log(value);
+// }
+// doArrow('Передай 5')
+
+const doArrow = (value) => {
+	console.log(value);
+}
+doArrow('Передай 5');
