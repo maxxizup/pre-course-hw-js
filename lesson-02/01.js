c186c4f352952e027377b311b6cf1b5fe51851d4
maxxizup <maksimkaxd1@gmail.com>
2024-04-02 01:34:48 +1000

    hw 01: one more fix

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,9 +1,4 @@
 // Переделай функцию на стрелочный синтаксис
 
-// function doArrow(value) {
-// 	console.log(value);
-// }
-// doArrow('Передай 5')
-
 const doArrow = value => console.log(value);
 doArrow('Передай 5');
